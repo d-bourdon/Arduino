cda780d4338bcc10d10171626e88fb2ca50f2894
galixX <dbourdon@student.42.fr>
2015-09-27 00:58:17 +0200

    rendu final des fichiers

--- meteo/receve_meteo.ino
+++ meteo/receve_meteo.ino
@@ -5,7 +5,8 @@
 float temperature;
 float hum;
 int tempbrute;
-int humbrute;
+long humbrute;
+int detector;
 
 void setup()
 {
@@ -18,20 +19,24 @@ void setup()
 
 void loop()
 {
-   int buf[2];
+   long buf[4];
    uint8_t buflen = sizeof(buf);
 
    if (vw_get_message((uint8_t *)buf, &buflen)) 
    {
+   //on print juste le résultat du buf, le raspberry le redécoupe quand il l'enregistre
      Serial.print(buf[0]);
      Serial.println(" :");
-     tempbrute=((buf[0]) * 0.0001);
-     humbrute=((buf[0]) - (tempbrute * 10000));
+     tempbrute=((buf[0]) * 0.00001);
+     humbrute=((buf[0]) - (tempbrute * 100000));
      temperature=(tempbrute * 0.01);
-     hum=(humbrute * 0.01);
+     hum=(humbrute * 0.001);
+     detector = (humbrute - (hum * 100));
      Serial.print(temperature);
      Serial.print(" / ");
      Serial.print(hum);
+     Serial.print(" / ");
+     Serial.print(detector);
      Serial.println();
 
    }
--- meteo/send_meteo.ino
+++ meteo/send_meteo.ino
@@ -4,42 +4,68 @@
 #include <VirtualWire.h>
 #include "DHT.h"
 #define DHTPIN 8     // what pin we're connected to
-#define DHTTYPE DHT22   // DHT 11
+#define DHTTYPE DHT22   // DHT 11 
 DHT dht(DHTPIN, DHTTYPE);
 int ledPin = 13;
- 
-int report[2];
+
+long report[2];
 //float temp=22.35;
 int tmp1;
-int hum1;
-int total_int;
- 
- 
+long hum1;
+int out_detector;
+int detector;
+int range;
+long total_int;
+
+// ----------------------
+//constante du rainsensor
+
+const int sensorMin = 0;     // sensor minimum
+const int sensorMax = 1024;  // sensor maximum
+
 void setup()
 {
    dht.begin();
-   Serial.begin(9600);  
-   Serial.println("setup");
- 
-   vw_setup(2000);  
+   Serial.begin(9600);   
+   //Serial.println("setup");
+  
+   vw_setup(2000);   
 }
- 
+
 void loop()
 {
   float temp = dht.readTemperature();
-   tmp1 = (temp * 100);
-   Serial.println(tmp1); //2440-> OK
+   tmp1 = (temp * 1000);
+  // Serial.println(tmp1);
   float hum = dht.readHumidity();
-   hum1 = (hum * 100);   //4100-> OK
-   Serial.println(hum1);
-   total_int = ((tmp1)*10000);
-   Serial.println(total_int);  //20608-> Euh nop :p 24400000
-   total_int = (total_int + hum1);
-   Serial.println(total_int); //24708-> OK il fait l'adition déjà: 24404100
+   hum1 = (hum * 1000L);
+  // Serial.println(hum1);
+   total_int = (tmp1*10000L);
+  // Serial.println(total_int);
+   out_detector = analogRead(A0);
+  //  Serial.println(out_detector);
+   int range = map(out_detector, sensorMin, sensorMax, 0, 4);
+   // Serial.println(range);
+  switch (range) {
+ case 0:    // pleut beaucoup
+    detector = 4;
+    break;
+ case 1:    // pleut
+    detector = 3;
+    break;
+ case 2:    // un peu de pluie
+    detector = 2;
+    break;
+ case 3:    // pas de pluie
+    detector = 1;
+    break;
+  }
+  // Serial.println(detector);
+   total_int = (total_int + hum1 + detector);
    report[0] = total_int;
- 
+  // Serial.println(report[0]);
    vw_send((uint8_t *)report, sizeof(report));
-   vw_wait_tx();
+   vw_wait_tx(); 
    
    delay(5000);
 }
