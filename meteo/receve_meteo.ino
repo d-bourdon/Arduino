529866daca4021016cb9ed52c031ef86dd616662
root <root@localhost.localdomain>
2015-09-27 12:37:58 +0200

    Version final de la réception + commentaire

--- meteo/receve_meteo.ino
+++ meteo/receve_meteo.ino
@@ -1,17 +1,18 @@
-﻿/* -------------------------------------------- */
+/* -------------------------------------------- */
 /* Script de récéption et affichage des données */
+/* Par GalixX [-] https://github.com/galixX/    */
 /* -------------------------------------------- */
 #include <VirtualWire.h>
-float temperature;
-float hum;
-int tempbrute;
-long humbrute;
-int detector;
+float temperature; // température : 32,50
+float hum; // humidité : 42,00
+int tempbrute; // temperature sans virgule : 3250
+long humbrute; // hum sans virgule : 4200
+int detector; // detecteur de pluie
 
 void setup()
 {
    Serial.begin(9600);  
-    Serial.println("setup");
+//   Serial.println("Start !"); //debug only -> on retire cette ligne pour éviter qu'il l'enregistre dans nos donnée
 
    vw_setup(2000);  
     vw_rx_start();      
@@ -24,20 +25,23 @@ void loop()
 
    if (vw_get_message((uint8_t *)buf, &buflen)) 
    {
-   //on print juste le résultat du buf, le raspberry le redécoupe quand il l'enregistre
-     Serial.print(buf[0]);
-     Serial.println(" :");
-     tempbrute=((buf[0]) * 0.00001);
-     humbrute=((buf[0]) - (tempbrute * 100000));
-     temperature=(tempbrute * 0.01);
-     hum=(humbrute * 0.001);
-     detector = (humbrute - (hum * 100));
-     Serial.print(temperature);
-     Serial.print(" / ");
-     Serial.print(hum);
-     Serial.print(" / ");
-     Serial.print(detector);
-     Serial.println();
+//on print juste le résultat du buf, le raspberry le redécoupe quand il l'enregistre. Le résultat est récuperé en écoutant la sortie du port avec le raspberry.
+//ex:  3250|4200|1
+//     --1-|--2-|3
+// 1 = température brute, 2 = humidité brute, 3 = état du détécteur de pluie
+     Serial.println(buf[0]);
+//     Serial.println(" :"); //debug only
+//     tempbrute=((buf[0]) * 0.00001); //debug only
+//     humbrute=((buf[0]) - (tempbrute * 100000)); //debug only
+//     temperature=(tempbrute * 0.01); //debug only
+//     hum=(humbrute * 0.001); //debug only
+//     detector = (humbrute - (hum * 100)); //debug only
+//     Serial.print(temperature); //debug only
+//     Serial.print(" / "); //debug only
+//     Serial.print(hum); //debug only
+//     Serial.print(" / "); //debug only
+//     Serial.print(detector); //debug only : ici on a un problème de calcule surement a cause des longs.
+//     Serial.println(); //debug only
 
    }
 }
